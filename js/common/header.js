cc300b13a89ce9c03b63d9646c3b1ec9c786a421
minyoungovo <minyoungovo@gmail.com>
2023-12-18 23:41:21 +0900

    글자수정/인스타그램 이미지수정/헤더 스크롤 시 흰 배경화면 추가_231218

---
+++ js/common/header.js
@@ -0,0 +1,15 @@
+$(function(){
+    var $header = $('#header'); 
+    var $page = $('#family-site'); 
+    var $window = $(window);
+    var pageOffsetTop = $page.offset().top;
+    
+    $window.resize(function(){ //반응형 시 다시 계산
+      pageOffsetTop = $page.offset().top;
+    });
+    
+    $window.on('scroll', () => { //스크롤 시
+      var scrolled = $window.scrollTop() >= pageOffsetTop; //스크롤된 상태; true or false
+      $header.toggleClass('header-scroll', scrolled);
+    });
+  });
