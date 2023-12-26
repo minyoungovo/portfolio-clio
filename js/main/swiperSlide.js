02cc1d6b57b06739599997f2d34ffaf8c50c3343
minyoungovo <minyoungovo@gmail.com>
2023-12-26 23:22:59 +0900

    스크롤, 메인 이미지 스와이프 수정

--- js/main/swiperSlide.js
+++ js/main/swiperSlide.js
@@ -1,8 +1,4 @@
-
-
 $(function(){
-    
-    
     swiper();
 
     $(window).resize(function(){ 
@@ -12,10 +8,20 @@ $(function(){
     function swiper(){
         const visual_swiper = new Swiper('#visual .swiper-container',{
             effect : "fade",
-            spaceBetween: 30,
+            // spaceBetween: 30,
+            slidesPerView: "auto",
             centeredSlides: true,
-            // loop : true,
-            rewind: true,
+            loop: false,
+            loopAdditionalSlides : 1,
+            // delay :0,
+            // loop: true, // 반복 여부
+            // slideToClickedSlide: true, //클릭하면서 마지막 요소일때 loop으로 실행 할수 있습니다.
+            //centeredSlides: true, //가운데로 정렬하겠다
+            centeredSlides: false, //가운데 정렬 x
+            allowTouchMove : false, //마우스 드래그엔드랍 여부
+             rewind: true,
+            //loopedSlides:4,
+            //followFinger:true,
             autoplay: {
               delay: 2500,
               disableOnInteraction: false,
@@ -32,6 +38,8 @@ $(function(){
 
         const recommend_swiper = new Swiper('#recommend .swiper-container',{
             slidesPerView: 'auto',
+            // slidesPerView: '4',
+			// paginationClickable: true,
             loop:true,
             navigation: {
                 nextEl: '.swiper-button-next',
